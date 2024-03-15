4222eb64c8ac30e763b95c23d6df7eaeefd9115b
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-15 09:29:02 +0200

    use state to change favorite

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 export default function Card() {
-  const contact = { profile: "../src/assets/react.svg",
-  phone: "+34 (342345300)",
-  email: "email@email.cc",
-  isFavorite: false,
-}
+  const [contact, setContact] = useState({
+    profile: "../src/assets/react.svg",
+    phone: "+34 (342345300)",
+    email: "email@email.cc",
+    isFavorite: false,
+  });
+  function handleClick() {
+    setContact((prevContact => ({ ...prevContact, isFavorite: !prevContact.isFavorite })));
+  }
   let favoriteIcon = contact.isFavorite
     ? "../src/assets/fav-1.png"
     : "../src/assets/fav-2.png";
   return (
-    <div className="card">
+    <div className="card" onClick={handleClick}>
       <img src={contact.profile} alt="card-image" />
       <img src={favoriteIcon} alt="star-image" className="favorite-icon" />
       <h3>John Doe</h3>
